7c319c2fde83bc626f4936a3db75baf2e078aab4
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2024-11-01 15:21:36 +0000

    Base implementation

---
+++ j_unilist/src/main/java/com/uninorte/Node.java
@@ -0,0 +1,37 @@
+package com.uninorte;
+
+public class Node {
+    private int index;
+    private Object data;
+    private Node nextNode;
+    private Node prevNode;
+
+    public Node(int index, Object data){
+        this.index = index;
+        this.data = data;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public Node getNextNode() {
+        return nextNode;
+    }
+
+    public Node getPrevNode() {
+        return prevNode;
+    }
+
+    public void setPrevNode(Node node){
+        this.prevNode = node;
+    }
+
+    public void setNextNode(Node node){
+        this.nextNode = node;
+    }
+}
